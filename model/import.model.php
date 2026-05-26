79dde818edcbf17f16039f2c622eba3db4b0a3e9
GampongGlitch <272003664+GampongGlitch@users.noreply.github.com>
2026-05-26 21:20:43 +0700

    edit select data slip and bill

--- model/import.model.php
+++ model/import.model.php
@@ -21,13 +21,26 @@ class ImportModel
     }
     public static function getSlip($id)
     {
-        $sql = "SELECT t1.bill_id, t1.product_code, t2.product_name, t3.member_name, 
-                   t1.price, t1.qty, t1.total, t1.discount, t1.transaction_date AS `datetime`, t4.username 
-            FROM `transaction` t1
-            LEFT JOIN product t2 ON t1.product_code = t2.product_code
-            LEFT JOIN member t3 ON t1.member_id = t3.id
-            LEFT JOIN users t4 ON t1.user_id = t4.id
-            WHERE t1.bill_id = '$id' AND t1.is_type = 'นำเข้า'";
+        $sql = "SELECT t1.bill_id AS bill_id,
+                t1.product_code AS product_code,
+                t2.product_name AS product_name,
+                t3.member_name AS member_name,
+                t1.price AS price,
+                t1.qty AS qty,
+                t1.total AS total,
+                t1.discount AS discount,
+                t1.transaction_date AS `datetime`,
+                t4.username AS username
+            FROM 
+                `transaction` t1
+            LEFT JOIN
+                product t2 ON t1.product_code = t2.product_code
+            LEFT JOIN
+                member t3 ON t1.member_id = t3.id
+            LEFT JOIN
+                users t4 ON t1.user_id = t4.id
+            WHERE
+                bill_id = '$id' AND t1.is_type = 'นำเข้า'";
 
         $query = Backend::MySQL()->query($sql);
 
--- model/sale.model.php
+++ model/sale.model.php
@@ -44,13 +44,27 @@ class SaleModel
     public static function getSlip($id)
     {
         // 1. ดึงข้อมูลรายการสินค้าทั้งหมด
-        $sql = "SELECT t1.bill_id, t1.product_code, t2.product_name, t3.member_name, 
-                   t1.price, t1.qty, t1.total, t1.discount, t1.transaction_date AS `datetime`, t4.username 
-            FROM `transaction` t1
-            LEFT JOIN product t2 ON t1.product_code = t2.product_code
-            LEFT JOIN member t3 ON t1.member_id = t3.id
-            LEFT JOIN users t4 ON t1.user_id = t4.id
-            WHERE t1.bill_id = '$id' AND t1.is_type IN ('ขาย', 'เบิก', 'นำเข้า')";
+        $sql = "SELECT
+                t1.bill_id AS bill_id,
+                t1.product_code AS product_code,
+                t2.product_name AS product_name,
+                t3.member_name AS member_name,
+                t1.price AS price,
+                t1.qty AS qty,
+                t1.total AS total,
+                t1.discount AS discount,
+                t1.transaction_date AS `datetime`,
+                t4.username AS username
+            FROM 
+                `transaction` t1
+            LEFT JOIN
+                product t2 ON t1.product_code = t2.product_code
+            LEFT JOIN
+                member t3 ON t1.member_id = t3.id
+            LEFT JOIN
+                users t4 ON t1.user_id = t4.id
+            WHERE
+                t1.bill_id = '$id' AND t1.is_type IN ('ขาย', 'เบิก', 'นำเข้า')";
 
         $query = Backend::MySQL()->query($sql);
 
